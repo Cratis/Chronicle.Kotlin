4a3ee917293e921dc380eefd0288d7968a8133d8
Einar <einari@me.com>
2026-08-04 20:40:03 +0200

    Bump chronicle-contracts dependency to 16.13.4
    Pulls in Webhooks, Jobs, EventStoreSubscriptions, and ExternalServices
    gRPC stubs needed for upcoming client modules.

--- Source/build.gradle.kts
+++ Source/build.gradle.kts
@@ -8,7 +8,7 @@ group = "io.cratis"
 version = providers.gradleProperty("version").getOrElse("0.0.0-SNAPSHOT")
 
 val coroutinesVersion = "1.9.0"
-val chronicleContractsVersion = "16.3.1"
+val chronicleContractsVersion = "16.13.4"
 val dnsJavaVersion = "3.6.5"
 
 dependencies {
