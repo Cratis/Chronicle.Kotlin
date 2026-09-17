9dccfb4b7bfff358081ffa90487bb1add43e63b9
Einar <einari@me.com>
2026-09-17 19:26:02 +0200

    Upgrade chronicle-contracts to 19.0.1
    Picks up Chronicle's 19.0.1 kernel release, including the fix for a
    connect-time compatibility check that could reject a client which had only
    gained fields since the server's release (Cratis/Chronicle#4058).

--- Source/build.gradle.kts
+++ Source/build.gradle.kts
@@ -8,7 +8,7 @@ group = "io.cratis"
 version = providers.gradleProperty("version").getOrElse("0.0.0-SNAPSHOT")
 
 val coroutinesVersion = "1.11.0"
-val chronicleContractsVersion = "18.5.0"
+val chronicleContractsVersion = "19.0.1"
 val dnsJavaVersion = "3.6.5"
 val classGraphVersion = "4.8.195"
 val openTelemetryVersion = "1.66.0"
