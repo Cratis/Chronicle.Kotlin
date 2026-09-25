b0fa0bfa16c87f1e988031a2ab8a4d30b7578f98
woksin <sindrewilting@gmail.com>
2026-09-25 17:56:52 +0200

    Compile published JVM libraries against Spring Boot's coroutines baseline

--- Testing/build.gradle.kts
+++ Testing/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 group = "io.cratis"
 version = providers.gradleProperty("version").getOrElse("0.0.0-SNAPSHOT")
 
-val coroutinesVersion = "1.11.0"
+val coroutinesVersion = "1.10.2"
 
 dependencies {
     api(project(":Source"))
